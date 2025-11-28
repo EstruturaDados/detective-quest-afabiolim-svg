5808d00670f42d9263bfd52959aa272782b29a56
afabiolim-svg <afabiolim@gmail.com>
2025-11-28 19:37:51 +0000

    SALA#

--- DETECTIVE-1.C
+++ DETECTIVE-1.C
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// Estrutura: Sala (árvore binária)
+typedef struct Sala {
+    char *nome;
+    struct Sala *esq;
+    struct Sala *dir;
+} Sala;
+
+Sala* criarSala(const char *nome) {
+    Sala *s = (Sala*) malloc(sizeof(Sala));
+    s->nome = strdup(nome);
+    s->esq = s->dir = NULL;
+    return s;
+}
